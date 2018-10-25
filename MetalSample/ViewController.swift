2b678000064a8f2d811527253b8abc4cbda53733
Mamunul Mazid <mamunul.mazid@gmail.com>
2018-10-25 14:00:27 +0600

    Initial Commit

---
+++ MetalSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MetalSample
+//
+//  Created by Paradox Lab on 25/10/18.
+//  Copyright © 2018 Paradox Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
